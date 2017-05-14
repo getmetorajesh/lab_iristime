88680c0b14298ba305625db8fa098e03f0a005cd
rajesh krishnan <getmetorajesh@gmail.com>
2017-05-14 10:53:16 +1000

    addconfig

--- server/service.js
+++ server/service.js
@@ -4,32 +4,32 @@ const express = require('express');
 const service = express();
 const request = require('superagent');
 const moment = require('moment');
-
+const config = require('../config');
 
 service.get('/service/:location', (req, res, next) => {
+  console.log('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.googleGeoApiKey);
+  request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + config.googleGeoApiKey, (err, response) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
 
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=GEO_API_KEY', (err, response) => {
-        if(err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
+    const location = response.body.results[0].geometry.location;
+    const timestamp = +moment().format('X');
 
-        const location = response.body.results[0].geometry.location;
-        const timestamp = +moment().format('X');
+    request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.googleTimeApiKey, (err, response) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
 
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=TIMEZONE_API_KEY', (err, response) => {
-            if(err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            
-            const result = response.body;
+      const result = response.body;
 
-            const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
-        
-            res.json({result: timeString});
-        });
+      const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
+
+      res.json({ result: timeString });
     });
+  });
 
 });
 
